3bfc0107ce13e351a4c1dab15dc60452432676e3
dylst <cdylan127@gmail.com>
2023-03-29 23:35:05 -0700

    Setup recharts and key performance indicators ui

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -4,50 +4,49 @@ import Row2 from './Row2';
 import Row3 from './Row3';
 
 const gridTemplateLargeScreens = `
-    "a b c"
-    "a b c"
-    "a b c"
-    "a b f"
-    "d e f"
-    "d e f"
-    "d h i"
-    "g h i"
-    "g h j"
-    "g h j"
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
 `;
 
 const gridTemplateSmallScreens = `
-    "a"
-    "a"
-    "a"
-    "a"
-    "b"
-    "b"
-    "b"
-    "b"
-    "c"
-    "c"
-    "c"
-    "d"
-    "d"
-    "d"
-    "e"
-    "e"
-    "f"
-    "f"
-    "f"
-    "g"
-    "g"
-    "g"
-    "h"
-    "h"
-    "h"
-    "h"
-    "i"
-    "i"
-    "j"
-    "j"
-
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
 `;
 
 const Dashboard = () => {
@@ -62,7 +61,7 @@ const Dashboard = () => {
         isAboveMediumScreens
           ? {
               gridTemplateColumns: 'repeat(3, minmax(370px, 1fr))',
-              gridTemplateRows: 'repeat(10, minmax(60px, 1 fr))',
+              gridTemplateRows: 'repeat(10, minmax(60px, 1fr))',
               gridTemplateAreas: gridTemplateLargeScreens,
             }
           : {
